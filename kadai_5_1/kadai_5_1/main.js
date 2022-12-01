1cc1dec19cfb0e67b04e75d6e20562d521b7d845
mori-takanri <115381859+mori-takanri@users.noreply.github.com>
2022-12-02 06:13:55 +0900

    Add files via upload

---
+++ kadai_5_1/kadai_5_1/main.js
@@ -0,0 +1,12 @@
+//何か変 
+
+let bookCloseNav = document.querySelector(".book-close-btn");
+let bookOpenNav = document.querySelector(".book-open-btn");
+let menuBar = document.querySelector(".menu-bar");
+
+bookCloseNav.onclick = (){
+  menuBar.classList.toggle("active");
+  bookCloseNav.classList.toggle("active");
+  bookOpenNav.classList.toggle("active");
+
+};
